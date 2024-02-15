bdf9fbd64842c525dc72983fddb71d8f03a8dc05
levbui <lev.bui@avepoint.com>
2024-02-15 22:59:14 +0700

    Add menu user in navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Toaster } from 'sonner';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ConvexClientProvide } from '@/components/providers/convex-provider';
 
@@ -32,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${inter.className} h-screen`}>
         <ConvexClientProvide>
           <ThemeProvider
             attribute='class'
@@ -41,6 +42,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey='notion-theme-2'
           >
+            <Toaster position='bottom-center' />
             {children}
           </ThemeProvider>
         </ConvexClientProvide>
