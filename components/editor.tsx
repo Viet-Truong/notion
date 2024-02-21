1dbc36b1819bdc139e45c1b93ec4e81674575bb0
levbui <lev.bui@avepoint.com>
2024-02-21 15:39:14 +0700

    handle editor note

---
+++ components/editor.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { BlockNoteEditor } from '@blocknote/core';
+import { BlockNoteView, useBlockNote } from '@blocknote/react';
+import '@blocknote/react/style.css';
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+  });
+  return (
+    <div>
+      <BlockNoteView editor={editor} theme={resolvedTheme === 'dark' ? 'dark' : 'light'} />
+    </div>
+  );
+};
+
+export default Editor;
